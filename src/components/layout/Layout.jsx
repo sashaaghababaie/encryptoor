b6a74e283348ea2291e2068591132c35452efec6
Sasha Aghababaie <sasha.aghababaie@gmail.com>
2026-02-20 06:02:34 +0330

    Delete src directory

--- src/components/layout/Layout.jsx
+++
@@ -1,7 +0,0 @@
-export default function Layout({ children }) {
-  return (
-    <div className="w-screen h-screen p-4 flex justify-center overflow-hidden">
-      <div className="w-full max-w-[800px]">{children}</div>
-    </div>
-  );
-}
